86950c2cfa3e6e72e5e0a57ca9cddb7d331c8478
Hciehshcj672 <161240119+Hciehshcj672@users.noreply.github.com>
2024-09-17 09:03:59 +0600

    Update run_gotty.sh

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-/usr/local/bin/gotty --permit-write --reconnect /bin/bash && docker run -d --network=host --name=9hits 9hitste/app /nh.sh --token=882530ba3f2d8d77451cdc028cd700c8  --system-session --allow-crypto=no
+docker run -d --network=host --name=9hits 9hitste/app /nh.sh --token=882530ba3f2d8d77451cdc028cd700c8  --system-session --allow-crypto=no
